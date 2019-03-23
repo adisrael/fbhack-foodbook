f8328028c5f2fdf89150d848587a92ac5bbad52a
Alexander Israel <adisrael@uc.cl>
2019-03-23 18:11:16 -0300

    profile data

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
-
+import userData from '../data/users.js'
 
 class Profile extends Component {
+  constructor() {
+        super()
+        this.state = {
+            users: userData
+        }
+        // this.handleChange = this.handleChange.bind(this)
+    }
   render() {
+    const userArray = this.state.users.map(user => <User key={user.id} user={user}/>)
+
     return (
-        <div>
-           <h1>Profile</h1>
-        </div>
+      <div>
+          {userArray}
+      </div>
     );
   }
+}
+
+function User(props) {
+  const styles = {
+    text_align: 'center'
+  }
+  const image_style = {
+    width: '50%',
+    height: 'auto'
+  }
+  return (
+    <div class="container">
+      <br></br>
+      <h1 style={styles}>{props.user.username}</h1>
+      <img src={props.user.image} alt="image" style={image_style}/>
+      <div className="stats">
+        <p>Followers: {props.user.followers}</p>
+        <p>Following: {props.user.following}</p>
+        <p>{props.user.tags}</p>
+      </div>
+      <div className="description">
+        <p>{props.user.description}</p>
+      </div>
+      <div className="playlists">
+        <h2>Playlists</h2>
+      </div>
+      <div className="recipes">
+        <h2>Recipes</h2>
+      </div>
+    </div>
+  )
 }
 
 export default Profile;
---
+++ src/data/users.js
@@ -0,0 +1,17 @@
+import logo from "../assets/adisrael.jpg"
+
+const userData = [
+    {
+        id: 1,
+        username: "adisrael",
+        image: logo,
+        tags: ['italian', 'japanese'],
+        description: "Chilean, Food Enthusiast, Food Lover <3",
+        playlists: [],
+        recipes: [],
+        followers: '103k',
+        following: 1025,
+    },
+]
+
+export default userData
