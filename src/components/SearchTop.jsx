1034c19a9d59837d530cfd4d1fae468551b061e5
Yoav Navon <yanavon@uc.cl>
2019-03-23 16:44:21 -0300

    Add search components files

---
+++ src/components/SearchTop.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class SearchTop extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Explore</h1>
+        </div>
+    );
+  }
+}
+
+export default SearchTop;
