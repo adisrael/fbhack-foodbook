1e8e9838d3eb12ed5f29d8026a9519fdfed2344a
Yoav Navon <yanavon@uc.cl>
2019-03-24 01:27:59 -0300

    Add margin on menu

--- src/components/SearchContent.jsx
+++ src/components/SearchContent.jsx
@@ -4,7 +4,7 @@ import SquareGrid from './Square/SquareGrid'
 export default class SearchContent extends Component {
   render() {
     return (
-      <div>
+      <div style={{marginBottom:'65px'}}>
         <SquareGrid search={true} title="Cuisine" cells={['Peruvian','Italian', 'Japanese','French']}/>
         <SquareGrid search={true} title="Type" cells={['Desert','Lunch','Breakfast','Snack']}/>
         <SquareGrid search={true} title="Other" cells={['Desert','Lunch','Breakfast','Snack']}/>
