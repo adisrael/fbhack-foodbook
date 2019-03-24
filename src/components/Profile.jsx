c0f10a3fa0e7152627a44e9829f52a53d88da201
Alexander Israel <adisrael@uc.cl>
2019-03-23 21:05:30 -0300

    profile como deberia ser

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import userData from '../data/users.js'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import MasterList from './Lists/MasterList';
 
 class Profile extends Component {
   constructor() {
@@ -10,44 +14,97 @@ class Profile extends Component {
         // this.handleChange = this.handleChange.bind(this)
     }
   render() {
-    const userArray = this.state.users.map(user => <User key={user.id} user={user}/>)
+    let user_id = 1
+    const user = this.state.users[user_id]
 
     return (
       <div>
-          {userArray}
+          <User user={user}/>
       </div>
     );
   }
 }
 
 function User(props) {
-  const styles = {
-    text_align: 'center'
+  const background = {
+    backgroundColor: 'White'
+  }
+  const title_style = {
+    textAlign: 'center',
+    fontSize: '2.5em',
   }
   const image_style = {
+    display: 'block',
     width: '50%',
-    height: 'auto'
+    // height: '50%',
+    // width: '50%',
+    // maxWidth: '50%',
+    height: 'auto',
+    left: '25%',
+    borderRadius: '50%',
+    position: 'relative',
+  }
+  const desc_style = {
+    marginTop: '0.75em',
+    textAlign: 'center',
+    fontWeight: 'normal',
+    color: '#555',
+    fontSize: '1em',
+  }
+  const stats_style = {
+    textAlign: 'center',
+    marginTop: '0.25em',
+    lineHeight: '0.1em',
+  }
+  const stats_text = {
+    fontWeight: 'normal',
+    color: '#888',
+    fontSize: '0.8em'
+  }
+  const stats_val = {
+    fontWeight: 'bold',
+    color: 'black'
   }
   return (
-    <div class="container">
+    <Container style={background}>
       <br></br>
-      <h1 style={styles}>{props.user.username}</h1>
+      <h1 style={title_style}>{props.user.username}</h1>
       <img src={props.user.image} alt="image" style={image_style}/>
-      <div className="stats">
-        <p>Followers: {props.user.followers}</p>
-        <p>Following: {props.user.following}</p>
-        <p>{props.user.tags}</p>
-      </div>
-      <div className="description">
+      <br></br>
+      <div style={desc_style}>
         <p>{props.user.description}</p>
       </div>
+      <Row >
+        <Col xs={4}>
+          <div style={stats_style}>
+            <p style={stats_val}>{props.user.playlists.length}</p>
+            <p style={stats_text}>Playlists</p>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div style={stats_style}>
+            <p style={stats_val}>{props.user.followers}</p>
+            <p style={stats_text}>Followers</p>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div style={stats_style}>
+            <p style={stats_val}>{props.user.following}</p>
+            <p style={stats_text}>Following</p>
+          </div>
+        </Col>
+      </Row>
+
+      {/* <Row>
+        <p>{props.user.tags}</p>
+      </Row> */}
       <div className="playlists">
-        <h2>Playlists</h2>
+        <MasterList title='Playlists'></MasterList>
       </div>
       <div className="recipes">
-        <h2>Recipes</h2>
+        <MasterList title='Recipes'></MasterList>
       </div>
-    </div>
+    </Container>
   )
 }
 
