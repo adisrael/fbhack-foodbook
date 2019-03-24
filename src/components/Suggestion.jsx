5868883661b62f89911d35925c60bc20aa5afb6c
Yoav Navon <yanavon@uc.cl>
2019-03-24 01:16:15 -0300

    Add ingredients tags

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 import SquareGrid from './Square/SquareGrid'
+import Tag from './Tag'
 
 export default class Suggestion extends Component {
     constructor(props){
         super(props)
-        this.state = {current: ['Fruits','Vegetables','Meats','Diary'], default:true}
+        this.state = {
+            current: ['Fruits','Vegetables','Meats','Diary'],
+            default:true,
+            selected: {}
+        }
         this.default = ['Fruits','Vegetables','Meats','Diary']
         this.subgroups = {
             Fruits: ['Banana','Apple','Pear','Watermelon','Strawberry'],
@@ -15,11 +24,19 @@ export default class Suggestion extends Component {
         }
         this.clickHandler = this.clickHandler.bind(this)
         this.backHandler = this.backHandler.bind(this)
+        this.deleteHandler = this.deleteHandler.bind(this)
+
     }
     clickHandler(category){
-        //console.log(category)
         if (this.subgroups[category]){
             this.setState({current: this.subgroups[category], default:false})
+        }
+        else{
+            this.setState((prevState,props)=>{
+                let selected = prevState.selected
+                selected[category] = true
+                return {selected}
+            })
         }
     }
 
@@ -27,13 +44,52 @@ export default class Suggestion extends Component {
         this.setState({current:this.default, default:true})
     }
 
+    deleteHandler(category){
+        this.setState((prevState,props)=>{
+            let selected = prevState.selected
+            selected[category] = false
+            return {selected}
+        })
+    }
+
     render() {
+        let selected = []
+        for (const key in this.state.selected) {
+            if (this.state.selected.hasOwnProperty(key)) {
+                if (this.state.selected[key]){
+                    selected.push(
+                        <Tag deleteHandler={this.deleteHandler} title={key}/>
+                    )
+                }
+            }
+        }
+        const box = {
+            border: 'solid black',
+            width:'100%',
+            margin: '5px',
+            borderRadius: '5px',
+        }
+        let element = (<div style={box}>
+        {selected}
+        </div>)
         return (
         <div>
             <Container>
+            
             <h1 style={{textAlign:'center'}}>New Ideas</h1>
-            <SquareGrid default={this.state.default} cells={this.state.current} clickHandler={this.clickHandler} backHandler={this.backHandler}></SquareGrid>
-
+            <Row>
+              {selected.length ? element : null}  
+            </Row>
+            <SquareGrid 
+                default={this.state.default}
+                cells={this.state.current}
+                clickHandler={this.clickHandler}
+                backHandler={this.backHandler}
+                selectHandler={this.selectHandler}
+                deleteHandler={this.deleteHandler}
+            >
+            </SquareGrid>
+            <Button style={{width:'100%', marginTop:'50px'}} variant='success'>GO!</Button>
             </Container>
         </div>
         )
