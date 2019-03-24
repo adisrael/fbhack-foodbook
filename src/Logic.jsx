78d660ac288ab55d93c409b803ae3a6319543029
Yoav Navon <yanavon@uc.cl>
2019-03-24 03:54:51 -0300

    Add results

--- src/Logic.jsx
+++ src/Logic.jsx
@@ -6,17 +6,22 @@ import Playlist from './components/Playlist.jsx'
 import Recipe from './components/Recipe.jsx'
 import Menu from './components/Menu.jsx'
 import Suggestion from './components/Suggestion.jsx';
+import Results from './components/Results.jsx';
 
 
 class Logic extends Component {
     constructor(props){
         super(props)
-        this.state = {index: 3}
+        this.state = {index: 3, ingredients: []}
         this.menuClickHandler = this.menuClickHandler.bind(this)
+        this.ingredientsHandler = this.ingredientsHandler.bind(this)
     }
 
     menuClickHandler(index){
         this.setState({index})
+    }
+    ingredientsHandler(ingredients){
+        this.setState({ingredients})
     }
 
   render() {
@@ -38,7 +43,10 @@ class Logic extends Component {
         element = <Recipe/>
     }
     else if (this.state.index === 5){
-        element = <Suggestion/>
+        element = <Suggestion menuClickHandler={this.menuClickHandler} ingredientsHandler={this.ingredientsHandler}/>
+    }
+    else if (this.state.index === 6){
+        element = <Results ingredients={this.state.ingredients} menuClickHandler={this.menuClickHandler}/>
     }
     return (
         <div>
